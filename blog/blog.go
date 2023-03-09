92395fb5f25da046189ca5a2bc2fa6e55d6452f1
sjdhome <sjdhome@foxmail.com>
2023-03-09 17:31:43 +0800

    Seperate web processing part from blog.go

--- blog/blog.go
+++ blog/blog.go
@@ -1,63 +1,15 @@
 package blog
 
 import (
-	"embed"
-	"html/template"
-	"io/fs"
 	"kurisu/web"
-	"log"
-	"net/http"
-	"strings"
 )
 
-type blog struct{}
-
-//go:embed www
-var _wwwFS embed.FS
-var wwwFS, _ = fs.Sub(_wwwFS, "www")
-var wwwServer = http.StripPrefix("/blog", http.FileServer(http.FS(wwwFS)))
-
-func (b blog) Method() string {
-	return "GET"
+type blog struct {
+	Title string
 }
 
-func (b blog) Path(path string) bool {
-	path = strings.TrimPrefix(path, "/")
-	path = strings.TrimSuffix(path, "/")
-	f, err := wwwFS.Open(path)
-	if err == nil {
-		s, err := f.Stat()
-		if err == nil && s.IsDir() {
-			return false
-		}
-	}
-	return strings.HasPrefix(path, "blog")
-}
-
-func (b blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p := r.URL.Path
-	p = strings.TrimPrefix(p, "/")
-	p = strings.TrimSuffix(p, "/")
-	if p == "blog" || p == "blog/index.html" {
-		tmpl, err := template.ParseFS(wwwFS, "index.html")
-		if err != nil {
-			log.Println("\t", err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-		err = tmpl.Execute(w, b)
-		if err != nil {
-			log.Println("\t", err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-		return
-	}
-	wwwServer.ServeHTTP(w, r)
-}
-
-var route = blog{}
+var b = blog{}
 
 func New(msgBus chan string) {
-	web.RegisterRoute(&route)
+	web.RegisterRoute(&b)
 }
