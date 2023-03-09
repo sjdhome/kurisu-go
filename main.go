85dc7a1213217e3eb48a02c47287352acf91dc95
sjdhome <sjdhome@foxmail.com>
2023-03-09 17:30:46 +0800

    Seperate initLog() from main.go

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"kurisu/aboutme"
 	"kurisu/blog"
 	"kurisu/terminal"
 	"kurisu/web"
-	"log"
 	"os"
 )
 
@@ -40,19 +38,4 @@ func main() {
 			os.Exit(0)
 		}
 	}
-}
-
-func initLog() {
-	// Send log to stdout and file.
-	logFile, err := os.OpenFile(*logFilename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Println("Failed to open log file.")
-		log.Fatalln(err)
-	}
-	defer logFile.Close()
-	mw := io.MultiWriter(os.Stdout, logFile)
-	log.SetOutput(mw)
-
-	log.SetPrefix("[kurisu] ")
-	log.Println("Starting kurisu...")
 }
