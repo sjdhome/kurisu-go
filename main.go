8223aadd4d317cff7e3c2713eb3afb3a18cce29f
sjdhome <sjdhoome@gmail.com>
2023-10-27 15:28:12 +0800

    Almost usable

--- main.go
+++ main.go
@@ -1,23 +1,200 @@
 package main
 
 import (
-	"net/http"
-	"log"
+	"database/sql"
+	"encoding/json"
 	"flag"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"net/http"
 	"os"
+	"regexp"
+	"strings"
 )
 
-type BlogPostHandler struct {}
+type BlogHandler struct {
+	blog *Blog
+}
 
-func (h BlogPostHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world"))
-	log.Println("Receive request")
+func (h BlogHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		isSinglePost, err := regexp.MatchString(`^\/blog\/post\/[A-Za-z\-]+\/`, r.URL.Path)
+		if err != nil {
+			log.Println("Unable to parse regular expression for URL.")
+			log.Println(err)
+			return
+		}
+		if isSinglePost {
+			readContent, err := regexp.MatchString(`^\/blog\/post\/[A-Za-z\-]+\/content\/`, r.URL.Path)
+			if err != nil {
+				log.Println("Unable to parse regular expression for URL.")
+				log.Println(err)
+				return
+			}
+			if readContent {
+				// Read single post content
+				tmp, _ := strings.CutPrefix(r.URL.Path, "/blog/post/")
+				id, _ := strings.CutSuffix(tmp, "/content/")
+				postContent, err := h.blog.GetPostContent(id)
+				log.Printf("%s is reading blog post %s content.\n", r.RemoteAddr, id)
+				if err != nil {
+					log.Printf("Unable to read blog post %s content.\n", id)
+					log.Println(err)
+					return
+				}
+				w.Write([]byte(postContent))
+			} else {
+				// Get single post
+				tmp, _ := strings.CutPrefix(r.URL.Path, "/blog/post/")
+				id, _ := strings.CutSuffix(tmp, "/")
+				post, err := h.blog.GetPost(id)
+				log.Printf("%s is reading blog post %s.\n", r.RemoteAddr, id)
+				if err != nil {
+					log.Printf("Unable to get blog post %s.\n", id)
+					log.Println(err)
+					return
+				}
+				j, err := json.Marshal(post)
+				if err != nil {
+					log.Printf("Unable to marshal blog post %s.\n", id)
+					log.Println(err)
+					return
+				}
+				w.Write(j)
+			}
+		} else if r.URL.Path == "/blog/post/" {
+			// Get all posts
+			log.Printf("%s is reading all blog posts.\n", r.RemoteAddr)
+			posts, err := h.blog.GetAllPosts()
+			if err != nil {
+				log.Println("Unable to get all blog posts.")
+				log.Println(err)
+				return
+			}
+			j, err := json.Marshal(posts)
+			if err != nil {
+				log.Println("Unable to marshal blog posts.")
+				log.Println(err)
+				return
+			}
+			w.Write(j)
+		}
+	} else {
+		// Not supported
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+type Blog struct {
+	db *sql.DB
+}
+
+type PostMetadata struct {
+	Id          string   `json:"id"`
+	Title       string   `json:"title"`
+	Created     string   `json:"created"`
+	Edited      string   `json:"edited"`
+	Author      string   `json:"author"`
+	Tags        []string `json:"tags"`
+	Description string   `json:"description"`
+	ShowComment bool     `json:"showComment"`
+	Visible     bool     `json:"visible"`
+	PinToTop    bool     `json:"pinToTop"`
+}
+
+func (b Blog) GetAllPosts() ([]PostMetadata, error) {
+	var posts []PostMetadata
+	rows, err := b.db.Query("SELECT id, title, created, edited, author, tags, description, showComment, visible, pinToTop FROM 'blog-post';")
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var (
+			id          string
+			title       string
+			created     string
+			edited      string
+			author      string
+			_tags       string
+			description string
+			showComment int
+			visible     int
+			pinToTop    int
+		)
+		if err := rows.Scan(&id, &title, &created, &edited, &author, &_tags, &description, &showComment, &visible, &pinToTop); err != nil {
+			return nil, err
+		}
+		var tags []string
+		err := json.Unmarshal([]byte(_tags), &tags)
+		if err != nil {
+			return nil, err
+		}
+		post := PostMetadata{
+			Id:          id,
+			Title:       title,
+			Created:     created,
+			Edited:      edited,
+			Author:      author,
+			Tags:        tags,
+			Description: description,
+			ShowComment: showComment != 0,
+			Visible:     visible != 0,
+			PinToTop:    pinToTop != 0,
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
+func (b Blog) GetPost(id string) (*PostMetadata, error) {
+	row := b.db.QueryRow("SELECT title, created, edited, author, tags, description, showComment, visible, pinToTop FROM 'blog-post' WHERE id = ?", id)
+	var (
+		title       string
+		created     string
+		edited      string
+		author      string
+		_tags       string
+		description string
+		showComment int
+		visible     int
+		pinToTop    int
+	)
+	if err := row.Scan(&title, &created, &edited, &author, &_tags, &description, &showComment, &visible, &pinToTop); err != nil {
+		return nil, err
+	}
+	var tags []string
+	err := json.Unmarshal([]byte(_tags), &tags)
+	if err != nil {
+		return nil, err
+	}
+	post := PostMetadata{
+		Id:          id,
+		Title:       title,
+		Created:     created,
+		Edited:      edited,
+		Author:      author,
+		Tags:        tags,
+		Description: description,
+		ShowComment: showComment != 0,
+		Visible:     visible != 0,
+		PinToTop:    pinToTop != 0,
+	}
+	return &post, nil
+}
+
+func (b Blog) GetPostContent(id string) (string, error) {
+	return "Example", nil
 }
 
 var logFile string
+var port int
+var databaseFile string
 
 func main() {
 	flag.StringVar(&logFile, "log-file", "", "Specify log storage directory.")
+	flag.IntVar(&port, "port", 3000, "HTTP server port.")
+	flag.StringVar(&databaseFile, "database-file", "kurisu.sqlite3", "SQLite database file.")
 	flag.Parse()
 	if logFile != "" {
 		f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -28,8 +205,23 @@ func main() {
 		log.SetOutput(f)
 	}
 
-	var blogPostHandler BlogPostHandler
-	http.Handle("/blog/post/", blogPostHandler)
-	log.Println("HTTP server started at :3000.")
-	log.Fatalln(http.ListenAndServe(":3000", nil))
+	db, err := sql.Open("sqlite3", databaseFile)
+	if err != nil {
+		log.Println("Unable to open database.")
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
+	blog := Blog{
+		db: db,
+	}
+	blogHandler := BlogHandler{
+		blog: &blog,
+	}
+	http.Handle("/blog/", blogHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusBadRequest)
+	})
+	log.Printf("HTTP server started at :%d.\n", port)
+	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
